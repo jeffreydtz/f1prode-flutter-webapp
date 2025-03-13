f966bb858ce3f5bfab26a6e33664bb5e25bbae3e
Jeffrey Dietz <jef_dietz@hotmail.com>
2025-03-13 13:59:20 -0300

    Update service worker version in flutter_bootstrap.js, adjust resource hashes in flutter_service_worker.js, and modify base href in index.html for improved routing and deployment flexibility.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "47af4110c98a9514cf2bd6f836e6eb5f",
+const RESOURCES = {"flutter_bootstrap.js": "6c41f727c13c778e2e352f8cb9892014",
 "version.json": "53c3a5c7c4d9eea8b2b2ffc0f0f196ea",
-"index.html": "b22d16d40667e0a9e5d4932e70528b18",
-"/": "b22d16d40667e0a9e5d4932e70528b18",
-"main.dart.js": "274289b04b8cb82405287347ba168a80",
+"index.html": "a9592689462df11c92eaa1220872f689",
+"/": "a9592689462df11c92eaa1220872f689",
+"main.dart.js": "c752c8830ba658d1bf99eba8cf07fc6c",
 "404.html": "0dcc54ba5e2b49f83306b931a3068b32",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
