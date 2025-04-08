243693082fe9f9f4ee12413e4a9f116329b36ad1
Jeffrey Dietz <jef_dietz@hotmail.com>
2025-04-08 11:34:09 -0300

    Update service worker version in flutter_bootstrap.js, adjust resource hashes in flutter_service_worker.js, and refine index.html for improved performance and consistency.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "16919d395671a8d561f3f7407b6954ba",
+const RESOURCES = {"flutter_bootstrap.js": "976c383ec0c6614d794368075b5533b6",
 "version.json": "53c3a5c7c4d9eea8b2b2ffc0f0f196ea",
-"index.html": "512f464908ca9ca3234270175591d15a",
-"/": "512f464908ca9ca3234270175591d15a",
-"main.dart.js": "74cccc9a64eea7998092881fb5467469",
+"index.html": "23f628d986d46c020a887c641b3271ae",
+"/": "23f628d986d46c020a887c641b3271ae",
+"main.dart.js": "798d718ecbf4b2fb868cea7e9120d642",
 "404.html": "0dcc54ba5e2b49f83306b931a3068b32",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
